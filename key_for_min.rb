edc36ff12b91652a0de80bcdd64c3866fe4ad30d
Benson Yu <bennywhyu@gmail.com>
2018-11-26 04:46:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest_key=""
+  
+  name_hash.collect do |key,value|
+   if value
+     
+    smallest_key=
+  end
+  
+  smallest_key
 end
